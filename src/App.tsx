398ffac2d58e9e22d10eef2d7cdcb377e16b32ce
Frédéric Laurier-Montpetit <flmontpetit@gmail.com>
2024-01-17 23:04:16 -0500

    implement super basic language switching

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { PDFViewer, StyleSheet } from '@react-pdf/renderer';
 import './App.css';
 import Resume from './components/resume';
 import registerFonts from './static/register-fonts';
+import { ChakraProvider } from '@chakra-ui/react';
+import LanguageSelector from './components/language-selector';
+import Lang from './models/lang';
+import i18next from 'i18next';
+import { useState } from 'react';
+import { initReactI18next } from 'react-i18next';
+import translation from './static/translation.json';
 
 const styles = StyleSheet.create({
   viewer: {
@@ -9,12 +16,31 @@ const styles = StyleSheet.create({
   },
 })
 
+i18next.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
+  resources: translation,
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+
 export default function App() {
   registerFonts();
+  const [lang, setLang] = useState<Lang>("en");
+
+  const onLanguageSwitch = (lang: Lang) => {
+    setLang(lang);
+    i18next.changeLanguage(lang);
+  }
 
   return (
-    <PDFViewer style={styles.viewer}>
-      <Resume />
-    </PDFViewer>
+    <ChakraProvider>
+      <LanguageSelector onLanguageSwitch={onLanguageSwitch} />
+      <PDFViewer style={styles.viewer}>
+        <Resume lang={lang} />
+      </PDFViewer>
+    </ChakraProvider>
   )
 }
---
+++ src/components/language-selector.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+import Lang from "../models/lang";
+
+
+interface LanguageSelectorProps {
+  onLanguageSwitch: (lang: Lang) => void
+}
+
+const LanguageSelector = (props: LanguageSelectorProps) => {
+  let [lang, setLang] = useState<Lang>("en");
+
+  const onClick = () => {
+    const newLang = lang == "en" ? "fr" : "en";
+    props.onLanguageSwitch(newLang);
+    setLang(newLang);
+  }
+
+  return (
+    <Button onClick={onClick}>
+      {lang == "en" ? "Français" : "English"}
+    </Button>
+  );
+}
+
+export default LanguageSelector;
